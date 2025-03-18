51db1e95826cf73f5acb3c212688326e432d384e
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2025-03-18 11:41:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-// First animation for welcome text
-gsap.from("#animated-text", {
-  x: -200,
-  opacity: 0,
-  duration: 1.5,
-  ease: "power2.out",
-  delay: 0.5,
-});
+// Call to action
 
-// Running text animation (Marquee Effect)
-function startRunningText() {
-  let text = document.querySelector("#running-text");
-  gsap.to(text, {
-    x: "-100%", // Moves the text fully to the left
-    duration: 50, // Time to complete one run
-    ease: "linear",
-    repeat: -1, // Infinite repeat
-    delay: 2,
-  });
-}
+gsap.to(".cta-card", { duration: 1.2, opacity: 1, y: 0, ease: "power3.out" });
 
-startRunningText();
+// What we do
+
+gsap.to(".what-we-do", { duration: 1.2, opacity: 1, y: 0, ease: "power3.out" });
+
+// Scrolling to top
 
 const scrollToTopBtn = document.getElementById("scrollToTop");
 
