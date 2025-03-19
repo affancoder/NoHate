97a41204f20e0a012cc56f3167aad323383eac79
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2025-03-19 15:53:05 +0530

    Add files via upload

--- register.js
+++ register.js
@@ -9,6 +9,8 @@ import {
   createUserWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 
+
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
